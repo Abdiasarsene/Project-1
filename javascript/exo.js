1990888cd641d533561bd468ebf6beb62c4caca9
Abdiasarsene <abdiasarsene@gmail.com>
2024-02-24 02:50:49 -1100

    Cours sur Javascript

--- javascript/exo.js
+++ javascript/exo.js
@@ -1,3 +1,41 @@
-let moncul = 10
-const mon = 'fuckyou'
-console.log(moncul)
+//Manipulation des chiffres
+let monage = 10
+
+let = monage1 = 12
+
+const num = 23
+const num2 = 34
+
+const totalnum =  num + num2
+
+console.log(monage)
+
+console.log(totalnum)
+
+let monage2 = 23
+
+let total = monage - monage1
+console.log(total)
+
+// Manipulation des caractères
+const prenom = "abdias"
+const prenom1 = ", abdiel"
+
+const tprenom = prenom + prenom1
+console.log(tprenom)
+
+//Manipulation des boléens
+let ageEtienne = true
+
+//Exerice sur OpenClassroom
+let Nbredelivrebiblio = 500
+const achatdeplus = 50
+const jetlivre = 10
+const rachalivre = 5
+
+const totalLivre = Nbredelivrebiblio + achatdeplus - jetlivre - jetlivre
+
+console.log(totalLivre)
+
+//Déclarez un objet
+let
