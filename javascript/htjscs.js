044ab2a7a620967777b12f1ff4f3ac63d840dc12
Abdiasarsene <abdiasarsene@gmail.com>
2024-02-24 19:17:09 -1100

    All files

--- javascript/htjscs.js
+++ javascript/htjscs.js
@@ -1,13 +1,1 @@
-// Ajouts des conditions affichant un pop-up
-const nomApp = Bonjour
-let nomUserz = prompt("Entrer le mot: "+nomApp)
-
-if(nomUserz===nomApp){
-    console.log("Good Job")
-} else{
-    console.log("Zut, reesayez")
-}
-
-//Créer un tableau
-let listMots =["Cachalot", "Pétunia", "Serviette"]
-let score= listMots.length
+let nav = getElementBy
